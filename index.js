8a3f24b865b248c18b21558372909a59eb4ed186
Martin Disch <martindisch@gmail.com>
2018-10-23 15:10:34 +0200

    Remove simple-git & docker-compose
    They're shit and it's easier to do this the simple way.

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const http = require('http');
 const webhook = require('github-webhook-handler')({
   path: '/', secret: process.env.CI_SECRET
 });
-const git = require('simple-git/promise')(process.env.CI_DIR);
-const compose = require('docker-compose');
+const exec = require('await-exec');
 
 http.createServer((req, res) => {
   webhook(req, res, () => {
@@ -18,13 +17,27 @@ webhook.on('error', (err) => {
 });
 
 webhook.on('push', async (event) => {
+  // Isolate branch from remote ref
   const hookBranch = event.payload.ref.split('/')[2];
   console.log(`Received a push event for branch ${hookBranch}`);
+  // Only continue if we're watching this branch
   if (hookBranch === process.env.CI_BRANCH) {
-    console.log(`Doing git pull on branch ${process.env.CI_BRANCH}`);
-    await git.pull('origin', process.env.CI_BRANCH);
-    console.log('Running docker-compose up --build');
-    await compose.buildAll({ cwd: process.env.CI_DIR });
-    await compose.upAll({ cwd: process.env.CI_DIR });
+    try {
+      // Attempt git pull
+      console.log(`Doing git pull on branch ${process.env.CI_BRANCH}`);
+      await exec(
+        `cd ${process.env.CI_DIR} && \
+        git checkout ${process.env.CI_BRANCH} && \
+        git pull`
+      );
+
+      // Attempt docker-compose
+      console.log('Running docker-compose');
+      await exec(`cd ${process.env.CI_DIR} && docker-compose up -d --build`);
+
+      console.log('New version deployed successfully');
+    } catch (err) {
+      console.error(`Unable to deploy\n${err.stderr}`);
+    }
   }
 });
